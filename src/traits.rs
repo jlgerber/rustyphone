1c8156875970c49f1e5fd0344e9c92ae204c83f9
Jonathan Gerber <jlgerber@gmail.com>
2020-11-24 10:54:57 -0800

    updates docstring for traits

--- src/traits.rs
+++ src/traits.rs
@@ -1,3 +1,5 @@
+/// A Selectable implements `select`, which simply returns a 
+/// comma separated list of columns.
 pub trait Selectable {
     /// Return the select comma separated list of columns 
     /// as a String 
