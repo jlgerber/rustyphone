686ddf7ab824f1f7a86ff42abe90a6871174ac3f
Jonathan Gerber <jlgerber@gmail.com>
2020-11-24 13:46:46 -0800

    adds phone to src/create

---
+++ src/create/phone .rs
@@ -0,0 +1,40 @@
+//! C in crud
+//! create
+
+//use crate::PersonView;
+use std::convert::AsRef;
+use sqlx::prelude::*;
+
+const CREATE_PHONE: &'static str = r"
+SELECT 
+    *
+FROM
+    addPhone($1, $2, $3, $4)
+";
+
+#[derive(FromRow)]
+struct Rval {
+    id: i32
+}
+pub async fn phone<I, J, K, L, M>(
+    pool: &sqlx::PgPool, 
+    login: I, 
+    number: J, 
+    category: K, 
+    location: L
+) -> Result<i32, sqlx::Error>
+where
+    I: AsRef<str>,
+    J: AsRef<str>,
+    K: AsRef<str>,
+    L: AsRef<str>,
+{
+    let row = sqlx::query(&CREATE_PERSON)
+    .bind(login.as_ref())
+    .bind(number.as_ref())
+    .bind(category.as_ref())
+    .bind(location.as_ref())
+    .fetch_one(pool).await?;
+    let Rval{id} = Rval::from_row(&row).unwrap();
+    Ok(id)
+}
