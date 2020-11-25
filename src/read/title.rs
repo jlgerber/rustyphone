c46afbec1412bbee7b6381d79191029d3b9bd31d
Jonathan Gerber <jlgerber@gmail.com>
2020-11-24 21:35:51 -0800

    swaps out JasonAdapter for uitls::JsonAdapter

--- src/read/title.rs
+++ src/read/title.rs
@@ -2,9 +2,11 @@
 use futures::TryStreamExt;
 use sqlx::FromRow;
 use serde::{Serialize, Deserialize};
+use crate::JsonAdapter;
+
 const QUERY: &'static str = r"
 SELECT 
-    row_to_json(r) AS titleview 
+    row_to_json(r) AS inner 
 FROM (
         SELECT 
             * 
@@ -20,22 +22,16 @@ pub struct TitleView {
     pub id: i32,
     pub name: String,
 }
-// just a way of extracting the json. We need to be able to implement
-// FromRow on something. (unless serde_json::Value has it implemented)
-#[derive(FromRow, Debug)]  
-struct JasonAdapter {
-    pub titleview: serde_json::Value
-}
 
 /// Given a reference to the PgPool and a QueryParam instance, look up the 
 /// matching values in the db and return a vector of json objects.
-pub async fn titleview(pool: &sqlx::PgPool) -> Result<Vec<serde_json::Value>, sqlx::Error> {    
+pub async fn query(pool: &sqlx::PgPool) -> Result<Vec<serde_json::Value>, sqlx::Error> {    
     let mut rval = Vec::new();
     let mut rows = sqlx::query(&QUERY)
                     .fetch(pool);
     while let Some(row) = rows.try_next().await? {
-        let JasonAdapter{titleview} =JasonAdapter::from_row(&row).unwrap();   
-        rval.push(titleview);
+        let JsonAdapter{inner} =JsonAdapter::from_row(&row).unwrap();   
+        rval.push(inner);
     }
     Ok(rval)
 }
